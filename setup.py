37764ea7d5a1ce85f9c39dee18ef454a215ec789
Ryan Nowakowski <ryan@bigfootnetworks.com>
2011-05-25 16:08:16 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = "django-jqgrid",
+    version = "r5",
+    author = "Gerry Eisenhaur",
+    author_email = "gerry@hiredhacker.com",
+    description  = ("django utilities allowing easy handling and configuration of jqGrids"),
+    license = "BSD",
+    python_modules = ['jqgrid'],
+)
+
